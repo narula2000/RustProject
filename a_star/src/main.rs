a32eaba0deb0bb6dfde7e27078ab63d530545053
Vikrom Narula <djnarula@gmail.com>
2020-12-07 14:32:06 +0700

    Commented and explain the code

--- a_star/src/main.rs
+++ a_star/src/main.rs
@@ -9,31 +9,8 @@ use std::hash::Hash;
 use std::ops::Sub;
 use std::usize;
 
-fn main() {
-    static GOAL: Pos = Pos(4, 7);
-    let result = astar(
-        &Pos(1, 1),
-        |p| p.moves(),
-        |p| p.distance(&GOAL) / 3,
-        |p| *p == GOAL,
-    );
-
-    println!("Passed: {:?}", result.expect("Passs?"));
-}
-
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos(i32, i32);
-
-fn abs_sub<T>(x: T, y: T) -> T
-where
-    T: Sub<Output = T> + PartialOrd,
-{
-    if x < y {
-        y - x
-    } else {
-        x - y
-    }
-}
 
 impl Pos {
     fn distance(&self, other: &Pos) -> u32 {
@@ -56,6 +33,87 @@ impl Pos {
         .map(|p| (p, 1))
         .collect()
     }
+}
+
+fn main() {
+    static GOAL: Pos = Pos(4, 7);
+    let result = astar(
+        &Pos(1, 1),
+        |p| p.moves(),
+        |p| p.distance(&GOAL) / 3,
+        |p| *p == GOAL,
+    );
+
+    println!("Passed: {:?}", result.expect("Passs?"));
+}
+
+fn astar<N, C, FN, IN, FH, FS>(
+    start: &N,
+    mut get_neighbor: FN,
+    mut heuristic: FH,
+    mut success: FS,
+) -> Option<(Vec<N>, C)>
+where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+    FH: FnMut(&N) -> C,
+    FS: FnMut(&N) -> bool,
+{
+    // Open Heap: Generate all possible moves for the curent node
+    let mut open = BinaryHeap::new();
+    open.push(SmallestCostHolder {
+        estimated_cost: heuristic(start),
+        cost: Zero::zero(),
+        index: 0,
+    });
+    // Closed Heap: Path of the best traversal
+    let mut parents: IndexMap<N, (usize, C)> = IndexMap::new();
+    parents.insert(start.clone(), (usize::max_value(), Zero::zero()));
+
+    while let Some(SmallestCostHolder { cost, index, .. }) = open.pop() {
+        // Get all neighbors and return if found End
+        let neighbors = {
+            let (node, &(_, c)) = parents.get_index(index).unwrap();
+            if success(node) {
+                let path = reverse_path(&parents, |&(p, _)| p, index);
+                return Some((path, cost));
+            }
+            if cost > c {
+                continue;
+            }
+            get_neighbor(node)
+        };
+        // Go through all the neighbors and map cost
+        for (neighbor, move_cost) in neighbors {
+            let new_cost = cost + move_cost;
+            let h_cost; // heuristic(&neighbor)
+            let idx; // index for neighbor
+            match parents.entry(neighbor) {
+                Vacant(map) => {
+                    h_cost = heuristic(map.key());
+                    idx = map.index();
+                    map.insert((index, new_cost));
+                }
+                Occupied(mut map) => {
+                    if map.get().1 > new_cost {
+                        h_cost = heuristic(map.key());
+                        idx = map.index();
+                        map.insert((index, new_cost));
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            open.push(SmallestCostHolder {
+                estimated_cost: new_cost + h_cost,
+                cost: new_cost,
+                index: idx,
+            });
+        }
+    }
+    None
 }
 
 struct SmallestCostHolder<K> {
@@ -103,66 +161,13 @@ where
     path.into_iter().rev().cloned().collect()
 }
 
-fn astar<N, C, FN, IN, FH, FS>(
-    start: &N,
-    mut successors: FN,
-    mut heuristic: FH,
-    mut success: FS,
-) -> Option<(Vec<N>, C)>
+fn abs_sub<T>(x: T, y: T) -> T
 where
-    N: Eq + Hash + Clone,
-    C: Zero + Ord + Copy,
-    FN: FnMut(&N) -> IN,
-    IN: IntoIterator<Item = (N, C)>,
-    FH: FnMut(&N) -> C,
-    FS: FnMut(&N) -> bool,
+    T: Sub<Output = T> + PartialOrd,
 {
-    let mut to_see = BinaryHeap::new();
-    to_see.push(SmallestCostHolder {
-        estimated_cost: heuristic(start),
-        cost: Zero::zero(),
-        index: 0,
-    });
-    let mut parents: IndexMap<N, (usize, C)> = IndexMap::new();
-    parents.insert(start.clone(), (usize::max_value(), Zero::zero()));
-    while let Some(SmallestCostHolder { cost, index, .. }) = to_see.pop() {
-        let moves = {
-            let (node, &(_, c)) = parents.get_index(index).unwrap();
-            if success(node) {
-                let path = reverse_path(&parents, |&(p, _)| p, index);
-                return Some((path, cost));
-            }
-            if cost > c {
-                continue;
-            }
-            successors(node)
-        };
-        for (_move, move_cost) in moves {
-            let new_cost = cost + move_cost;
-            let h_cost; // heuristic(&successor)
-            let idx; // index for successor
-            match parents.entry(_move) {
-                Vacant(map) => {
-                    h_cost = heuristic(map.key());
-                    idx = map.index();
-                    map.insert((index, new_cost));
-                }
-                Occupied(mut map) => {
-                    if map.get().1 > new_cost {
-                        h_cost = heuristic(map.key());
-                        idx = map.index();
-                        map.insert((index, new_cost));
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            to_see.push(SmallestCostHolder {
-                estimated_cost: new_cost + h_cost,
-                cost: new_cost,
-                index: idx,
-            });
-        }
+    if x < y {
+        y - x
+    } else {
+        x - y
     }
-    None
 }
